86c6661272e9eab4bc8315ab56d5c6f91055fc99
LaupWing <loc_strict@hotmail.com>
2021-11-27 18:25:42 +0100

    base

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
       <div className="flex w-screen h-screen bg-gray-800">
          <div className="m-auto w-80 rounded overflow-hidden bg-white h-52 shadow">
-            <h2 className="p-2 bg-indigo-500 text-white uppercase font-bold">Room</h2>
+            <h2 className="p-2 bg-indigo-500 text-white uppercase tracking-wider">Room</h2>
          </div>
       </div>
   );
