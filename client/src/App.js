3b4a91de955db74c711837b31a740aacbf13773d
LaupWing <loc_strict@hotmail.com>
2021-11-28 11:57:50 +0100

    Editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import {useState} from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
+import Home from './pages/Home'
+import Editor from './pages/Editor'
 
 function App() {
    // const [state, setstate] = useState(false)
@@ -9,6 +10,7 @@ function App() {
       <div className="flex w-screen h-screen bg-gray-800">
          <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/editor" element={<Editor/>}/>
          </Routes>
       </div>
    );
