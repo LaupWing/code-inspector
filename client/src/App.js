f463de78bf59e0d6f8d09d2584a1d999e115ed0c
LaupWing <loc_strict@hotmail.com>
2021-11-28 11:55:45 +0100

    Route works

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import {useState} from 'react'
+// import {useState} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
-   const [state, setstate] = useState(false)
+   // const [state, setstate] = useState(false)
    
    return (
       <div className="flex w-screen h-screen bg-gray-800">
-         <div className="m-auto w-80 flex flex-col rounded overflow-hidden bg-white h-36 shadow">
-            <h2 className="p-2 bg-indigo-500 text-white uppercase tracking-wider">Room</h2>
-            <div className="m-auto">
-               <button className="bg-indigo-500 rounded-full mr-2 w-28 capitalize py-0.5 text-white">Join Room</button>
-               <button className="bg-indigo-500 rounded-full w-28 capitalize py-0.5 text-white">Create Room</button>
-            </div>
-         </div>
+         <Routes>
+            <Route path="/" element={<Home/>}/>
+         </Routes>
       </div>
    );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+   return (
+      <div className="m-auto w-80 flex flex-col rounded overflow-hidden bg-white h-36 shadow">
+         <h2 className="p-2 bg-indigo-500 text-white uppercase tracking-wider">Room</h2>
+         <div className="m-auto">
+            <button className="bg-indigo-500 rounded-full mr-2 w-28 capitalize py-0.5 text-white">Join Room</button>
+            <button className="bg-indigo-500 rounded-full w-28 capitalize py-0.5 text-white">Create Room</button>
+         </div>
+      </div>
+   )
+}
+
+export default Home
