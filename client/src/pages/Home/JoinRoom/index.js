6536a951531d0129b4d382a00222c52665cc6c6f
LaupWing <loc_strict@hotmail.com>
2021-11-28 12:12:05 +0100

    joinroom

--- client/src/pages/Home/JoinRoom/index.js
+++ client/src/pages/Home/JoinRoom/index.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 const JoinRoom = () => {
    return (
-      <div>
-         
+      <div className="m-auto w-full p-2 flex flex-col">
+         <input 
+            type="text" 
+            className="bg-gray-300 w-full p-2 py-1 rounded" 
+            placeholder="Join a room!"
+         />
+         <div className="flex m-auto mt-3">
+            <button 
+               className="border-indigo-500 border-2 text-indigo-500 rounded-full mr-2 w-28 capitalize py-0.5"
+            >
+               cancel
+            </button>
+            <button 
+               className="bg-indigo-500 rounded-full w-28 capitalize py-0.5 text-white"
+            >
+               join
+            </button>
+         </div>
       </div>
    )
 }
